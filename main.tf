009ce141fdbdb03aa1cffb0cadc96fe6caeeb93d
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-04-24 14:31:07 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "~> 3.18.0"
+    }
+
+    nomad = {
+      source = "hashicorp/nomad"
+      version = "2.0.0-beta.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "nomad_cluster" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_organization
+    workspaces = {
+      name = "5_nomad-cluster"
+    }
+  }
+}
+
+provider "vault" {}
+
+data "vault_kv_secret_v2" "bootstrap" {
+  mount = data.terraform_remote_state.nomad_cluster.outputs.bootstrap_kv
+  name  = "nomad_bootstrap/SecretID"
+}
+
+provider "nomad" {
+  address = data.terraform_remote_state.nomad_cluster.outputs.nomad_public_endpoint
+  secret_id = data.vault_kv_secret_v2.bootstrap.data["SecretID"]
+}
+
+resource "nomad_job" "batch" {
+  hcl2 {
+    vars = {
+      job_name = var.job_name
+      node_pool = var.node_pool
+    }
+  }
+  jobspec = file("${path.module}/batch.hcl")
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "job_name" {
+  type        = string
+  description = "The name of your job"
+}
+
+variable "node_pool" {
+  type = string
+  description = "Where to run your batch job"
+}
+
+variable "cron_schedule" {
+  type = string
+  description = "Batch cron schedule"
+}
+
+variable "tfc_organization" {
+  type    = string
+  description = "The TFCB organization to use"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS and HCP region to create resources in"
+}
